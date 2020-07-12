f8ac468c49ad9126d3b823ff4e88cdba17d0aa96
raymondsumabat@gmail.com <raymondsumabat@gmail.com>
2020-07-12 18:52:59 +0800

    checkout page payment selection forms done so far

--- GAMESHOP/pages/checkout.php
+++ GAMESHOP/pages/checkout.php
@@ -2,19 +2,42 @@
   require "../view/user-header.php";
  ?>
 
-<form class="checkout-form" name="checkout-form" method="post">
-  Payment Method: <select>
-                      <option value="COD"> Cash on delivery </option>
-                      <option value="CC"> Credit Card/ Debit Card</option>
+<link rel="stylesheet" href="../css/design-checkout.css"/>
+
+<form class="payment-form" name="payment-form" method="post" action="checkout.php">
+  Payment Method: <select id="payment-type" name="payment-type" required>
+                      <option value="Cash on Delivery">Cash on Delivery</option>
+                      <option value="Credit Card/Debit Card">Credit Card/Debit Card</option>
                   </select>
-                  <input type="text" name="Credit Card Number" placeholder="Credit Card Number"/>
-                  <input type="text" name="Expiration Date" placeholder="Expiration Date"/>
-                  <input type="text" name="CVV" placeholder="CVV"/>
-                  <br />
-                  <input type="text" name="Shipping Address"/>
+                  <button type="submit" name="method-confirm">Confirm Payment Method</button>
 </form>
 
+<form class="checkout-form" name="checkout-form" method="post">
+<?php
+if (isset($_POST['method-confirm'])) {
+
+  echo $_POST['payment-type'];
+
+  if ($_POST['payment-type'] == "Credit Card/Debit Card") {
+
+ echo '<input type="text" name="Credit Card Number" placeholder="Credit Card Number" required/>
+       <input type="text" name="Expiration Date" placeholder="Expiration Date" required/>
+       <input type="number" name="CVV" placeholder="CVV" required/>
+       <br />
+       <input type="text" name="Shipping Address" placeholder="Shipping Address" required/>
+       <button type="submit" name="checkout-confirm">Confirm Checkout</button>';
+
+    } else if ($_POST['payment-type'] == "Cash on Delivery"){
+
+  echo '<br />
+        <input type="text" name="Shipping Address" placeholder="Shipping Address" required/>
+        <button type="submit" name="checkout-confirm">Confirm Checkout</button>';
+
+    }
+}
+?>
+</form>
 
 <?php
   require "../view/footer.php";
-?>
+ ?>
